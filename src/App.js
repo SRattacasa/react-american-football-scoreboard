1e58a647762f781f21bda04890a1a583eb301d4f
SRattacasa <38533+SRattacasa@users.noreply.github.com>
2020-04-22 16:46:19 -0400

    fin

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homescore, setHomescore] = useState(0);
   const [awayscore, setAwayscore] = useState(0);
-
-  
+    
 
   return (
     <div className="container">
